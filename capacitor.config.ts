c298b9062b04f7c745b70cc5b511eb86d4702ed2
elopes89 <manu021rj@gmail.com>
2023-08-09 02:36:32 -0300

    teste

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'capacitor',
+  webDir: 'dist/capacitor',
+
+};
+
+export default config;
